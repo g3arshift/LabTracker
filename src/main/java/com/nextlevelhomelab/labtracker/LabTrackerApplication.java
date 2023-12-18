ad32eb03a8b7cab60e4791805da78831d2ecea20
Gear Shift <gearshift@gearshiftgaming.com>
2023-12-17 21:03:15 -0500

    Initial commit

---
+++ src/main/java/com/nextlevelhomelab/labtracker/LabTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.nextlevelhomelab.labtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nextlevelhomelab/labtracker/LabTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextlevelhomelab.labtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
