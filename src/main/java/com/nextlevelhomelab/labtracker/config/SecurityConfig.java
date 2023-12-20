d729712de1d4be6c273b4e1136f1abfa644a7813
Gear Shift <gearshift@gearshiftgaming.com>
2023-12-19 22:02:34 -0500

    Created several tables in DB and added security context

---
+++ src/main/java/com/nextlevelhomelab/labtracker/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.nextlevelhomelab.labtracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //TODO: Remove. Allows anything to auth.
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+    //TODO: Remove. Allows web console access to DB.
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+
+}
